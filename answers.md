8da905465f9b3c9c2b7edaed5a928cb53334f46e
Nir Hilman <nir.hilman@amdocs.com>
2022-03-05 23:36:11 +0200

    add concurrency

--- answers.md
+++ answers.md
@@ -1,1 +1,3 @@
 The time complexity in the worst case of the code is number of messages(m) multiply the longest message words(n). or O(nm). We can improve it by adding concurrency while each message processed by another thread.
+
+I improved the time complexity by adding concurrency.
--- src/main/java/keepers/Keepers.java
+++ src/main/java/keepers/Keepers.java
@@ -1,12 +1,23 @@
 package keepers;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Keepers {
 
+    ExecutorService executor = Executors.newFixedThreadPool(10);
+
     List<String> filterEmojies(List<String> messages) {
-        return messages.stream().map(m -> m.replaceAll("[\ud83c\udf00-\ud83d\ude4f]|[\ud83d\ude80-\ud83d\udeff]", ""))
-                .collect(Collectors.toList());
+        List<String> filteredMeaasges = new ArrayList<>();
+        messages.stream().forEach(m -> CompletableFuture
+                .supplyAsync(() -> {
+                    return m.replaceAll("[\ud83c\udf00-\ud83d\ude4f]|[\ud83d\ude80-\ud83d\udeff]", "");
+                }, executor)
+                .thenApply(f -> filteredMeaasges.add(f)));
+        return filteredMeaasges;
     }
+
 }
